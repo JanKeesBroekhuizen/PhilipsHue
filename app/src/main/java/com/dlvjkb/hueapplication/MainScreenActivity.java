ca6413fe34f690aa4e4cd51ec99cf5e2c24ba72e
Dave-Avans <dl.visser@student.avans.nl>
2020-11-22 10:55:50 +0100

    [Init]: Created the main fragment and set a goodday text.

--- app/src/main/java/com/dlvjkb/hueapplication/MainScreenActivity.java
+++ app/src/main/java/com/dlvjkb/hueapplication/MainScreenActivity.java
@@ -3,6 +3,7 @@ package com.dlvjkb.hueapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainScreenActivity extends AppCompatActivity {
 
